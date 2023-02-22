34e11b8c0198e89b2ba3ac53a6f738d331b208d2
ArseneNgenzi <arsenengenzi@gmail.com>
2023-02-22 16:38:51 +0200

    working on the navbar

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+import { urlFor } from 'lib/client'
+import product from 'sanityecommerce/schemas/product'
+
+const HeroBanner = ({bannerData}) => {
+  console.log(bannerData)
+  const {buttonText, desc, largeText1,  midText,  smallText, image} = bannerData
+  return (
+    <div>
+      <div className="hero-banner-container">
+        <div>
+          <p className="beats-solo">{midText}</p>
+          <h3>{midText}</h3>
+          <h1>{largeText1}</h1>
+          <img src={urlFor(image)} alt="headphones" className="hero-banner-image" />
+          <div>
+            <Link href={`/product/${product}`}>
+              <button>{buttonText}</button>
+            </Link>
+            <div className="desc">
+              <h5>{desc}</h5>
+              <p>{smallText}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
---
+++ components/Product.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { urlFor } from "lib/client";
+import Link from "next/link";
+import Image from "next/image";
+
+const Product = ({ product }) => {
+	const { image, name, slug, price, details } = product;
+	console.log(product);
+	return (
+		<div>
+			<Link href={`/product/${slug.current}`}>
+				<div className="product-card">
+					<img
+						src={urlFor(image && image[0])}
+						alt=""
+						width={250}
+						height={250}
+						className="product-image"
+					/>
+          <p className="product-name">{name}</p>
+          {/* <p>{details}</p> */}
+          <p className="product-price">${price}</p>
+				</div>
+			</Link>
+		</div>
+	);
+};
+
+export default Product;
